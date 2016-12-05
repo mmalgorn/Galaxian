aa04e24277c3038be4fe56fda174c40e98d95e3d
quent <quent@LAPTOP-L80M1JC2>
2016-12-05 16:15:39 +0100

    ;

--- src/v0/Game.java
+++ src/v0/Game.java
@@ -1,5 +1,43 @@
 package v0;
 
-public class Game {
+import java.awt.Point;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+// credits: 
+//	random color: http://stackoverflow.com/questions/4246351/creating-random-colour-in-java
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Space root = new Space();
+		ArrayList<Element> ennemys = new ArrayList<Element>();
+		int i,nbEnnemy = 8;
+
+		Element defender =  new Defender(new Point(350,450));
+		root.addElement(defender);
+		
+		for(i=0;i<nbEnnemy;i++){
+			ennemys.add(new FireInvaders(new Point(100+(i*75),100)));
+			root.addElement(ennemys.get(i));
+		}
+		root.start();
+  		while (true) {
+			try {
+				Thread.sleep(300);
+				Iterator<Element> iter = root.elementIterator();
+				
+				/*while(iter.hasNext()){
+					Element m = iter.next();
+					if(m.getUrl().equals("./img/mechant03.png"))
+						m.setPosition(new Point());
+				}*/
+				root.repaint();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
